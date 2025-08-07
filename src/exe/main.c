1dec5b710f4bcf414c2a550a5fc64bae7da92663
cesar <teltzents@gmail.com>
2025-08-07 06:28:51 -0300

    fix more indentation issues

--- src/exe/main.c
+++ src/exe/main.c
@@ -34,10 +34,11 @@ void handle_game_error(enum ToeError te) {
 int main() {
   constexpr auto input_len = 4;
   constexpr char input_req[] = "input should be 2 length long\n";
-	char input[input_len];
-	struct Toe game = {};
-	enum ToeError error = {};
-	
+  
+  char input[input_len];
+  enum ToeError error = NO_ERROR;
+  struct Toe game = {};
+  
   while (true) {
     toe_presentation(&game);
     handle_game_error(error);  
@@ -81,5 +82,5 @@ int main() {
       printf("winner: %s!\n", toe_winner(victory));
       return EXIT_SUCCESS;
     }
-	}
+  }
 }
